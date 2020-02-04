4ebff7164b3b84849c63898eeda6a5b2c2f133f1
FinalBoss <finalb@projectfinalboss.com>
2020-02-03 22:12:57 -0500

    drastic changes in approach to responsiveness

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 //Responsive
 
+w3.addClass('.Main', 'w3-center w3-cell-row')
+w3.addClass('#headingOrange', 'w3-center')
+w3.addClass('div #celebrationBoi', 'w3-image w3-center')
+w3.addClass('nav', 'w3-bar')
+w3.addClass('a', 'w3-bar-item w3-large')
+w3.addClass('.contentM', 'w3-middle w3-center')
+w3.addClass('#Join', 'w3-center w3-large') 
