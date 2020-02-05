5ef7ae12d4727de19e64811f4fc0aa376dc4193b
FinalBoss <finalb@projectfinalboss.com>
2020-02-05 17:02:27 -0500

    A lot of changes, sorry can't describe them all...
    Ok, just the important ones:
    
    -Add a modal for the sign up overlay
    -Fix a lot of responsiveness issues

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 //Responsive
 
-w3.addClass('.Main', 'w3-center w3-cell-row')
-w3.addClass('#headingOrange', 'w3-center')
-w3.addClass('div #celebrationBoi', 'w3-image w3-center')
-w3.addClass('nav', 'w3-bar')
-w3.addClass('a', 'w3-bar-item w3-large')
+w3.addClass('button', 'w3-container w3-center')
+w3.addClass('p', 'w3-padding')
+w3.addClass('p,h1,h2,h3,h4,h5,h6,a,button', 'w3-notoFam')
+w3.addClass('.Main', ' w3-cell-row')
+w3.addClass('.heading h1', ' w3-xlarge')
+w3.addClass('#headingOrange', 'w3-cell-rest')
+w3.addClass('#headingOrange h1', ' w3-left-align w3-padding')
+w3.addClass('#invitePpl h1, p', 'w3-center w3-padding')
+//w3.addClass('div #celebrationBoi', '')
+w3.addClass('nav', 'w3-bar w3-left w3-padding')
+w3.addClass('a', 'w3-bar-item w3-medium')
 w3.addClass('.contentM', 'w3-middle w3-center')
-w3.addClass('#Join', 'w3-center w3-large') 
+w3.addClass('#Join', 'w3-center w3-large')
+w3.addClass('.joinBtn', ' w3-small ')
+w3.addClass('#btnContainer', 'w3-center   w3-bar') 
+w3.addClass('.footerM, section', 'w3-padding w3-panel')
+
+//Styles
+
+w3.addStyle('.w3-notoFam', 'font-family', 'Noto Sans')
+w3.addStyle('button, h1', 'font-weight', 'bold')
